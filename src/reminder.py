eb52517f1cb91649e7fceee31611ceec49ddf092
radhika-star <radha.agarwal3001@gmail.com>
2023-03-28 12:37:16 +0000

    first commit

--- src/reminder.py
+++ src/reminder.py
@@ -2,6 +2,6 @@ class PrefixedReminder:
     """This class acts as a base class for other types of reminders.
     Classes that subclass it should override the `self.text` property
     """
-    def __init__(self, prefix="Hey, don't forget to "):
+    def __init__(self, prefix="please"):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
